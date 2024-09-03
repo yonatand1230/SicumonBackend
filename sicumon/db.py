95c5fe4e4fa2700f1b8fdc9dc1950616bc29ff15
yonatand1230 <yonatand1230@gmail.com>
2024-09-03 20:12:52 +0300

    Optimize code, remove duplicate functions

--- sicumon/db.py
+++ sicumon/db.py
@@ -4,34 +4,39 @@ from .sicum import Sicum
 from boto3.dynamodb.conditions import Attr
 from datetime import datetime
 
+AWS_KEYID = os.environ['AWS_KEYID']
+AWS_SECRET = os.environ['AWS_SECRET']
+AWS_REGION = 'il-central-1'
+S3_ACCESSPOINT = os.environ['S3_ACCESSPOINT']
+
 def file_size(file) -> int:
     # Get (temp) file size in bytes
     size = file.seek(0,2)
     file.seek(0)
     return size
 
+def new_session():
+    session = boto3.Session(
+        aws_access_key_id=AWS_KEYID,
+        aws_secret_access_key=AWS_SECRET,
+        region_name=AWS_REGION
+    )
+
 class Db:
     def new_file(file: io.BytesIO, file_meta: Sicum) -> Sicum:
-        #load_dotenv() # Init .env
-        # Connect to AWS
-        session = boto3.Session(
-            aws_access_key_id=os.environ['AWS_KEYID'],
-            aws_secret_access_key=os.environ['AWS_SECRET'],
-            region_name='il-central-1'
-        )
-        # Connect to S3
-        resource = session.resource('s3', region_name='il-central-1')
+        session = new_session() # Open AWS Session
+
+        resource = session.resource('s3') # Connect to S3
         s3 = resource.meta.client
 
-        # Connect to DynamoDB
-        dynamodb = session.resource('dynamodb', region_name='il-central-1')
+        dynamodb = session.resource('dynamodb') # Connect to DynamoDB
         table = dynamodb.Table('Files')
         
-        # Get file size
-        file_meta.fileSize = file_size(file)
+        
+        file_meta.fileSize = file_size(file) # Get file size
 
         # Upload file to S3
-        file_key = f'files/{file_meta.fileName}'
+        file_key = f'files/{file_meta.fileName}' 
         s3.upload_fileobj(file, 'sicumon', file_key)
         
         # Add meta to DynamoDB
@@ -53,69 +58,23 @@ class Db:
         return file_meta
 
     def generate_file_url(file_key: str) -> str:
-        #load_dotenv() # Init .env
-
-        # Connect to AWS
-        session = boto3.Session(
-            aws_access_key_id=os.environ['AWS_KEYID'],
-            aws_secret_access_key=os.environ['AWS_SECRET'],
-            region_name='il-central-1'
-        )
-        # Connect to S3
-        resource = session.resource('s3', region_name='il-central-1')
+        resource = new_session().resource('s3') # Connect to S3
         s3 = resource.meta.client
-
-        # Connect to DynamoDB
-        dynamodb = session.resource('dynamodb', region_name='il-central-1')
-        table = dynamodb.Table('Files')
-        return s3.generate_presigned_url('get_object', Params={'Bucket':os.environ['S3_ACCESSPOINT'],'Key':file_key})
+        return s3.generate_presigned_url('get_object', Params={'Bucket':S3_ACCESSPOINT ,'Key':file_key})
     
     def get_file_meta(file_key: str) -> Sicum:
-        #load_dotenv() # Init .env
+        dynamodb = new_session().resource('dynamodb') # Connect to DynamoDB
+        table = dynamodb.Table('Files')
 
-        # Connect to AWS
-        print('connecting to aws..')
-        session = boto3.Session(
-            aws_access_key_id=os.environ['AWS_KEYID'],
-            aws_secret_access_key=os.environ['AWS_SECRET'],
-            region_name='il-central-1'
-        )
-        # Connect to S3
-        #resource = session.resource('s3', region_name='il-central-1')
-        #s3 = resource.meta.client
-
-        # Connect to DynamoDB
-        print('connecting to dynamodb..')
-        try:
-            dynamodb = session.resource('dynamodb', region_name='il-central-1')
-            table = dynamodb.Table('Files')
-            response = table.get_item(Key={'fileKey':file_key})
-        except Exception as e:
-            print(e)
-
-        print('getting item..')
-        item = response.get('Item')
-        if item: return Sicum.from_dict(item)
-        return None
+        response = table.get_item(Key={'fileKey': file_key})
+        item = response.get('Item') # Get Item
+        
+        if not item: return None
+        return Sicum.from_dict(item)
     
     def get_files_by_subject(subject: str, grade: int, Limit: int = 10, ExclusiveStartKey:str=None) -> dict:
-        #load_dotenv() # Init .env
-
-        # Connect to AWS
-        print('connecting to aws')
-        session = boto3.Session(
-            aws_access_key_id=os.environ['AWS_KEYID'],
-            aws_secret_access_key=os.environ['AWS_SECRET'],
-            region_name='il-central-1'
-        )
-        # Connect to S3
-        #print('connecting to s3')
-        #resource = session.resource('s3', region_name='il-central-1')
-        #s3 = resource.meta.client
-
-        # Connect to DynamoDB
         print('connecting to dynamodb')
-        dynamodb = session.resource('dynamodb', region_name='il-central-1')
+        dynamodb = new_session().resource('dynamodb')
         table = dynamodb.Table('Files')
         response = None
 
@@ -132,6 +91,7 @@ class Db:
                     FilterExpression=Attr('subject').eq(subject)&Attr('grade').eq(grade),
                     Limit=Limit
                 )
+            print(response)
         except Exception as e:
             print(e)
         
